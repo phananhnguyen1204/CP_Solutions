737e9378dff7b7ec9905b7947757e3afe88e59c8
Phan Anh <phananh12042003@gmail.com>
2024-09-21 11:10:00 -0400

    323. Number of Connected Components in an Undirected Graph

---
+++ Leetcode/Medium/323. Number of Connected Components in an Undirected Graph/323. Number of Connected Components in an Undirected Graph.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        res = 0
+        visited = set()
+        adj = [[] for _ in range(n)]
+
+        for a, b in edges:
+            adj[a].append(b)
+            adj[b].append(a)
+
+        def dfs(node):
+            visited.add(node)
+            for next_node in adj[node]:
+                if next_node not in visited:
+                    dfs(next_node)
+        
+        for i in range(n):
+            if i not in visited:
+                res += 1
+                dfs(i)
+        return res
