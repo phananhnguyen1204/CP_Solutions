4ee1bd9aadf41cbdb34cdbc6f6b32c51e04fff35
Phan Anh <phananh12042003@gmail.com>
2024-10-26 16:23:52 -0400

    277. Find the Celebrity

--- Leetcode/Medium/277. Find the Celebrity/277. Find the Celebrity.py
+++ Leetcode/Medium/277. Find the Celebrity/277. Find the Celebrity.py
@@ -12,8 +12,11 @@ class Solution:
 
             return True
 
-        for i in range(n):
-            if check(i):
-                return i
+        candidate = 0
+        for i in range(1, n):
+            if knows(candidate, i):
+                candidate = i
+        if check(candidate):
+            return candidate
         return -1
     
