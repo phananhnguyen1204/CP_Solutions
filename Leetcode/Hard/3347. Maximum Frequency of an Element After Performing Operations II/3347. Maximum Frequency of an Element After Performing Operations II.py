41122d007597f034fa9a18a79aab17d24ecdacb3
Phan Anh <phananh12042003@gmail.com>
2025-01-11 18:03:40 -0800

    3347. Maximum Frequency of an Element After Performing Operations II

---
+++ Leetcode/Hard/3347. Maximum Frequency of an Element After Performing Operations II/3347. Maximum Frequency of an Element After Performing Operations II.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int, numOperations: int) -> int:
+
+        freq = defaultdict(int)
+        points = set()
+        points_cover = defaultdict(int)
+        
+        for num in nums:
+            freq[num] += 1
+            points_cover[num-k] += 1
+            points_cover[num + k + 1] -= 1
+            points.update({num, num - k, num + k + 1})
+        
+        points_cover_so_far = 0
+        res = 0
+        for point in sorted(points):
+            points_cover_so_far += points_cover[point]
+            res = max(res, freq[point] + min(points_cover_so_far - freq[point], numOperations))
+
+        return res
